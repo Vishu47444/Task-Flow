10b3ad90ae62d5d83879a99e54a45f3c6e6953a9
vishu47444 <vishwajeet1729@gmail.com>
2025-05-17 00:28:33 +0530

    first commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = ({ onSubmit, onSwitchMode }) => {
   const [rememberMe, setRememberMe] = useState(false)
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
-  const url = "http://localhost:4000"
+  const url = "https://task-flow-2-pgai.onrender.com"
 
   // Auto-login
   useEffect(() => {
